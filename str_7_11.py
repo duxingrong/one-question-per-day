e6c64e5a1d6cc66e523228cf5fe5b07da6337be5
duxingrong <193900684@qq.com>
2024-07-12 18:03:53 +0800

    反转字符串

---
+++ str_7_11.py
@@ -0,0 +1,28 @@
+"""
+编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+
+不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。
+
+你可以假设数组中的所有字符都是 ASCII 码表中的可打印字符。
+"""
+from typing import List
+
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        stack = []
+        for char in s:
+            stack.append(char)
+        for i in range(len(s)):
+            s[i] = stack.pop()
+
+
+solution = Solution()
+# 定义一个字符串列表
+s = list("hello")
+print("Original string:", "".join(s))
+solution.reverseString(s)
+print("Reversed string:", "".join(s))
+       
