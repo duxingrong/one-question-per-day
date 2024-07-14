3e27e7a93a003f122f5fb323398366b122f00613
duxingrong <193900684@qq.com>
2024-07-15 00:45:53 +0800

    KMP

---
+++ str_7_14.py
@@ -0,0 +1,35 @@
+"""
+实现 strStr() 函数。
+
+给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回  -1。
+
+示例 1: 输入: haystack = "hello", needle = "ll" 输出: 2
+
+示例 2: 输入: haystack = "aaaaa", needle = "bba" 输出: -1
+"""
+from typing import List
+class Solution:
+    def getNext(self, next: List[int], s: str) -> None:
+        j = 0
+        next[0] = 0
+        for i in range(1, len(s)):
+            while j > 0 and s[i] != s[j]:
+                j = next[j - 1]
+            if s[i] == s[j]:
+                j += 1
+            next[i] = j
+    
+    def strStr(self, haystack: str, needle: str) -> int:
+        if len(needle) == 0:
+            return 0
+        next = [0] * len(needle)
+        self.getNext(next, needle)
+        j = 0
+        for i in range(len(haystack)):
+            while j > 0 and haystack[i] != needle[j]:
+                j = next[j - 1]
+            if haystack[i] == needle[j]:
+                j += 1
+            if j == len(needle):
+                return i - len(needle) + 1
+        return -1
