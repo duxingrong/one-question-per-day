4164fc07a0127ea3e77ff73626c0079e4e07b898
duxingrong <193900684@qq.com>
2024-07-15 10:14:01 +0800

    KMP

--- str_7_14.py
+++ str_7_14.py
@@ -9,21 +9,28 @@
 """
 from typing import List
 class Solution:
+    #制作一个前缀表的求法
     def getNext(self, next: List[int], s: str) -> None:
+        #初始化前缀表
         j = 0
         next[0] = 0
         for i in range(1, len(s)):
+            #当不匹配时，需要退回去
             while j > 0 and s[i] != s[j]:
                 j = next[j - 1]
+            #相等时，相等前后缀长度加一
             if s[i] == s[j]:
                 j += 1
             next[i] = j
-    
+    #开始查找
     def strStr(self, haystack: str, needle: str) -> int:
+        #判断needle是否为空
         if len(needle) == 0:
             return 0
+        #得到前缀表
         next = [0] * len(needle)
         self.getNext(next, needle)
+        #开始匹配
         j = 0
         for i in range(len(haystack)):
             while j > 0 and haystack[i] != needle[j]:
@@ -31,5 +38,5 @@ class Solution:
             if haystack[i] == needle[j]:
                 j += 1
             if j == len(needle):
-                return i - len(needle) + 1
+                return i - len(needle) + 1   #返回的是第一个元素的指针
         return -1
