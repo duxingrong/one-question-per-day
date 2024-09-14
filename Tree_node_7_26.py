1ee023a0dc5fcd6f87ca84ccd775ec6a435bfbc8
duxingrong <193900684@qq.com>
2024-09-14 21:19:59 +0800

    9.14

--- Tree_node_7_26.py
+++ Tree_node_7_26.py
@@ -1,59 +1,53 @@
-# 前序遍历-递归----可以理解为根左右----#
-
-
+"""
+二叉树的深度三种遍历
+递归的技巧:
+1.确定参数和返回值
+2.确定终止条件
+3.确定单层递归的逻辑
+"""
 from typing import List
-# 二叉树节点的定义
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-#preorder---前序----traversal-----遍历-----#
-class Solution:
-    def preorderTraversal(self, root: TreeNode) -> List[int]:  # 定义前序遍历方法，root为二叉树的根节点
-        res = []  # 用于存储遍历结果的列表
-        
-        def dfs(node):  # 定义深度优先搜索函数
-            if node is None:  # 如果节点为空，返回
-                return
-            
-            res.append(node.val)  # 访问节点，将节点值加入结果列表
-            dfs(node.left)  # 递归遍历左子树
-            dfs(node.right)  # 递归遍历右子树
-        
-        dfs(root)  # 从根节点开始进行深度优先搜索
-        return res  # 返回遍历结果列表
+# 二叉树的定义
+class TreeNode():
+    def __init__(self):
+        self.val=val
+        self.left=left
+        self.right=right
 
-
-
-
-# 中序遍历-递归----#-----左根右------#
-class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
-        res = []
-        
+# 前序遍历(中左右)
+#这个函数需要传出来什么呢?-->输入二叉树的头节点,传出来一个数组,
+class Solution():
+    def preorderTraversal(self,root:TreeNode) ->List[int]: 
+        # 定义一个空列表
+        res=[]
         def dfs(node):
-            if node is None:    #---- 这里的 return 是结束当前函数执行，并返回到上一个调用点----#
+            if node is None:
                 return
-            
-            dfs(node.left)
             res.append(node.val)
+            dfs(node.left)
             dfs(node.right)
         dfs(root)
         return res
-    
 
-
-#------后续遍历-----左右根------#
+# 中序遍历(左中右)
 class Solution():
-    def postorderTraversal(self,root:TreeNode)->List[int]:
+    def indexorderTraversal(self,root:TreeNode) ->List[int]:
         res=[]
-        #----递归函数-----#
-        def dfs(self,node):
+        def dfs(node):
             if node is None:
                 return 
+        dfs(node.left)
+        res.append(node.val)
+        dfs(node.right)
+        return res
+    
+# 后序遍历(左右中)
+class Solution():
+    def postorderTraversal(self,root:TreeNode) ->List[int]:
+        res=[]
+        def dfs(node):
+            if node is None:
+                return
             dfs(node.left)
             dfs(node.right)
             res.append(node.val)
-        dfs(root)
-        return res
+        return res 
