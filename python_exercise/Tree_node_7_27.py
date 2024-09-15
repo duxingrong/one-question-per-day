c0d8eda7a85ff3606035a60ed18e2cd08d8bdfb4
duxingrong <193900684@qq.com>
2024-09-15 14:56:29 +0800

    9-15

--- python_exercise/Tree_node_7_27.py
+++ python_exercise/Tree_node_7_27.py
@@ -1,75 +1,86 @@
 """
 二叉树的迭代遍历
 """
+
 from typing import List
 
+# 二叉树的定义
 class TreeNode():
-    def __init__(self,val:int,left:None,right=None):
+    def __init__(self,val=0,left=None,right=None):
         self.val=val
         self.left=left
         self.right=right
 
-#------前序遍历----根左右-----#
-class Solution:
-    def preorderTraversal(self,root:TreeNode)->List[int]:
-#-----首先看是否根节点为空----#
-        if  not root  :
+# 二叉树的前序遍历
+class Solution1():
+    def preorderTraversal(self, root: TreeNode)-> List[int]:
+    # 遍历的节点和处理的节点是同一个?
+        if not root :
             return []
-        stack=[root]
-        result=[]
-        """
-        这个循环的逻辑是，每次都会先传出根节点，然后将他的左右节点传进去
-        """
+        stack=[root] #用来存放处理节点
+        res=[] #用来存放结果
         while stack:
-            node=stack.pop()
-            result.append(node.val)
+            node = stack.pop()
+            # 中节点先处理
+            res.append(node.val)
+            # 右孩子先进栈
             if node.right:
                 stack.append(node.right)
+            # 左孩子先进栈
             if node.left:
                 stack.append(node.left)
-        return result
+        return res
 
+# 二叉树的后序遍历
+"""
+由前序是中左右,而后序是左右中,所以改变顺序然后反转数组即可!(因为处理节点和遍历节点是同一个)
+"""
 
-
-#--------后序遍历------左右根---------前序根左右，也可以做到根右左，然后反转，就是左右根！！！！-----#
-class Solution:
+class Solution2():
     def postorderTraversal(self,root:TreeNode)->List[int]:
-        if  not root :
-            return []
-        result=[]
-        stack=[root]
-        while stack:
-            node=stack.pop()
-            result.append(node.val)
-            if node.left:
-                stack.append(node.left)
-            if node.right:
-                stack.append(node.right)
-        return result[::-1]
-
-
-
-#------中序遍历，就是说是左根右------------#--因为访问和要执行的不是一个节点，所以要加入指针-------#
-class Solution:
-    def inorderTraversal(self, root: TreeNode) -> List[int]:
         if not root:
             return []
-        stack = []  # 不能提前将root结点加入stack中
-        result = []
-        cur = root#----借助了指针-----#
-        while cur or stack:
-            # 先迭代访问最底层的左子树结点
-            if cur:     
+        stack=[root]
+        res=[]
+        while stack:
+            node=stack.pop()
+            res.append(node.val)
+            if node.left:
+                stack.append(node.left)
+            if node.right:
+                stack.append(node.right)
+        res=res[::-1]
+        return res
+
+
+"""
+中序遍历由于访问节点和处理节点不是同一个,所以是不同的逻辑处理
+先一路向左,访问的节点全部入栈,如果为空,就从栈中取出元素进行处理,然后看是否有右孩子,如果有,加入栈,然后继续循环
+"""
+class Solution3():
+    def indexorderTravesal(self,root:TreeNode)->List[int]:
+        if not root:
+            retun []
+        res=[]
+        stack=[]
+        cur=root
+        while stack or cur:
+            # 先访问最深层的左子树节点:
+            if cur:
                 stack.append(cur)
-                cur = cur.left		
-            # 到达最左结点后处理栈顶结点    
-            else:		
+                cur=cur.left
+            # 为空则弹出节点进行处理:
+            else:
                 cur = stack.pop()
-                result.append(cur.val)
-                # 取栈顶元素右结点
-                cur = cur.right	
-        return result
-        
-        
+                res.append(cur.val)
+                if cur.right:
+                    stack.append(cur.right)
+                cur = cur.right
+        return res
+            
+
+
+
 
             
+
