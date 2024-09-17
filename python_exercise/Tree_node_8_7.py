3e362b3cbec009d5a8b962f0d398bdd8ce96987f
duxingrong <193900684@qq.com>
2024-09-17 20:38:54 +0800

    9-17

--- python_exercise/Tree_node_8_7.py
+++ python_exercise/Tree_node_8_7.py
@@ -1,26 +1,82 @@
 """
 计算给定二叉树的所有左叶子之和。
 """
+from typing import Optional
+from collections import deque
 class TreeNode():
-    def __init__(self,val:int,left:None,right:None):
+    def __init__(self,val=0,left=None,right=None):
         self.val=val
         self.left=left
         self.right=right
         
+"""
+迭代法:(层序遍历还是容易)
+"""
 
-class Solution:
-    def sumOfLeftLeaves(self, root):
-        if root is None:
+class Solution1():
+    def leftsum(self,root:Optional[TreeNode])->int:
+        if not root:
             return 0
-        if root.left is None and root.right is None:
+        result = 0
+        queue=deque([root])
+        while queue:
+            node = queue.popleft()
+            if node.left and node.left.left==None and node.left.right==None:
+                result+=node.left.val
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return result
+"""
+递归法,应该算是前序(中左右)
+终止条件,遇到叶子节点就返回
+单层逻辑,我们处理就是对于当前节点,如果他有node.left:那么就把值加给result,然后就去向下递归遍历左右节点
+"""
+
+
+class Solution2():
+    def __init__(self):
+        self.result=0
+
+    def leftsum(self,root:Optional[TreeNode]):
+        if not root:
             return 0
+        self.getsum(root)
+        return self.result
         
-        leftValue = self.sumOfLeftLeaves(root.left)  # 左
-        if root.left and not root.left.left and not root.left.right:  # 左子树是左叶子的情况
-            leftValue = root.left.val
-            
-        rightValue = self.sumOfLeftLeaves(root.right)  # 右
+    def getsum(self,node):
+        # 终止条件
+        if node.left ==None and node.right==None:
+            return 
+        # 单层逻辑
+        if node.left and node.left.left==None and node.left.right==None:
+            self.result+=node.left.val
+        if node.left:
+            self.getsum(node.left)
+        if node.right:
+            self.getsum(node.right)
+   
+    
+        
+# 创建测试二叉树
+# 例如，构建如下树：
+#       1
+#      / \
+#     2   3
+#   /   
+#  2    
+# /
+#5
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left=TreeNode(2)
+root.left.left.left=TreeNode(5)
 
-        sum_val = leftValue + rightValue  # 中
-        return sum_val
+# 计算并打印最小深度
+solution = Solution2()
+print( f"这个树的左叶子之和是{ solution.leftsum(root) }")        
+ 
+
 
