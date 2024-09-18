0667d5b9578a4bcb2a3458d1f717e9307360d4a3
duxingrong <193900684@qq.com>
2024-09-18 18:50:26 +0800

    9-18

--- python_exercise/Tree_node_8_11.py
+++ python_exercise/Tree_node_8_11.py
@@ -1,48 +1,55 @@
 """
-给定一个不含重复元素的整数数组。一个以此数组构建的最大二叉树定义如下：
-
+给定一个不含重复元素的整数数组.一个以此数组构建的最大二叉树定义如下：
 二叉树的根是数组中的最大元素。
 左子树是通过数组中最大值左边部分构造出的最大二叉树。
 右子树是通过数组中最大值右边部分构造出的最大二叉树。
 通过给定的数组构建最大二叉树，并且输出这个树的根节点。
 """
+from typing import List, Optional
 class TreeNode():
-    def __init__(self,val:int,left:None,right:None):
+    def __init__(self,val=0,left=None,right=None):
         self.val=val
         self.left=left
         self.right=right
-class Solution:
-    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
-        if len(nums) == 1:
-            return TreeNode(nums[0])
-        node = TreeNode(0)
-        # 找到数组中最大的值和对应的下标
-        maxValue = 0
-        maxValueIndex = 0
-        for i in range(len(nums)):
-            if nums[i] > maxValue:
-                maxValue = nums[i]
-                maxValueIndex = i
-        node.val = maxValue
-        # 最大值所在的下标左区间 构造左子树
-        if maxValueIndex > 0:
-            new_list = nums[:maxValueIndex]
-            node.left = self.constructMaximumBinaryTree(new_list)
-        # 最大值所在的下标右区间 构造右子树
-        if maxValueIndex < len(nums) - 1:
-            new_list = nums[maxValueIndex+1:]
-            node.right = self.constructMaximumBinaryTree(new_list)
-        return node
-#切片
 
-class Solution:
-    def constructMaximumBinaryTree(self, nums: List[int]) -> TreeNode:
-        if not nums:
+"""
+和给两个数组是一个道理:
+1.找到最大值,然后值赋给root.val.由max_index分割左数组和右数组,然后递归去构造下一层
+"""
+
+class Solution1():
+    def makeTree(self,num:List[int])->Optional[TreeNode]:
+        # 终止条件
+        if not num:
             return None
-        max_val = max(nums)
-        max_index = nums.index(max_val)
-        node = TreeNode(max_val)
-        node.left = self.constructMaximumBinaryTree(nums[:max_index])
-        node.right = self.constructMaximumBinaryTree(nums[max_index+1:])
-        return node
-        
+        # 创建第一层根节点
+        root_val=max(num)
+        root=TreeNode(root_val)
+        max_index = num.index(root_val)
+
+        # 分割数组去构造下一层
+        leftnum = num[:max_index]
+        rightnum = num[max_index+1:]
+        # 递归构造第二层
+        root.left = self.makeTree(leftnum)
+        root.right = self.makeTree(rightnum)
+        return root
+
+
+num = [5,4,8,2,3]
+
+solution=Solution1()
+root = solution.makeTree(num)
+# 打印节点值
+print(root.val)
+print(root.left.val if root.left else None)
+print(root.right.val if root.right else None)
+print(root.left.right.val if root.left and root.left.right else None)
+print(root.left.left.val if root.left and root.left.left else None)
+print(root.right.left.val if root.right and root.right.left else None)
+print(root.right.right.val if root.right and root.right.right else None)
+
+
+
+
+
