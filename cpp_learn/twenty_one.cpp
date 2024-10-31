b46aa7837a1d650b8def91f7c952949404f428ed
du <193900684@qq.com>
2024-10-31 13:23:38 +0800

    10-31

--- cpp_learn/twenty_one.cpp
+++ cpp_learn/twenty_one.cpp
@@ -13,3 +13,69 @@ end
 Rectangle area: 15.00
 Circle area: 12.56
 */
+
+#include <iomanip>
+#include <iostream>
+#include <string>
+#include <vector>
+// 抽象Shape类
+class Shape {
+public:
+  // 获取类型
+  virtual std::string GetType() const = 0;
+  // 计算面积
+  virtual double GetArea() const = 0;
+};
+// 两个子类
+class Rectangle : public Shape {
+public:
+  // 构造函数
+  Rectangle(int width, int height) : width(width), height(height) {}
+  // 计算面积
+  double GetArea() const override {
+    return static_cast<double>(width * height);
+  }
+  // 获取类型
+  std::string GetType() const override { return "Rectangle"; }
+
+private:
+  int width;
+  int height;
+};
+class Circle : public Shape {
+private:
+  int radius;
+
+public:
+  Circle(int radius) : radius(radius) {}
+
+  std::string GetType() const override { return "Circle"; }
+
+  double GetArea() const override { return 3.14 * radius * radius; }
+};
+
+int main() {
+  // 创建一个容器存放对象
+  std::vector<Shape *> shapes;
+  while (true) {
+    std::string type;
+    std::cin >> type;
+    if (type == "end") {
+      break;
+    } else if (type == "rectangle") {
+      int width, height;
+      std::cin >> width >> height;
+      shapes.push_back(new Rectangle(width, height));
+    } else if (type == "circle") {
+      int radius;
+      std::cin >> radius;
+      shapes.push_back(new Circle(radius));
+    }
+  }
+  // 范围for循环打印
+  for (const Shape *shape : shapes) {
+    std::cout << shape->GetType() << " area: " << std::fixed
+              << std::setprecision(2) << shape->GetArea() << std::endl;
+  }
+  return 0;
+}
